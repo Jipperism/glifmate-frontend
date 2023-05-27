0c1f99c2dcfb88fad3b1e9d66c19805e5a9292a1
jipstavenuiter <jipstavenuiter@gmail.com>
2023-05-27 12:50:05 +0200

    remove empty pages

--- components/plasmic/pg_staking/PlasmicNavHomepage.tsx
+++ components/plasmic/pg_staking/PlasmicNavHomepage.tsx
@@ -206,7 +206,6 @@ type NodeOverridesType<T extends NodeNameType> = Pick<
   PlasmicNavHomepage__OverridesType,
   DescendantsType<T>
 >;
-
 type NodeComponentProps<T extends NodeNameType> =
   // Explicitly specify variants, args, and overrides as objects
   {
